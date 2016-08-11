0b54f865dc7b2db5354c018fa0cc14da4920e168
Kong Q Mr (PG/R - Elec Electronic Eng) <qk00006@cvplws132.eps.surrey.ac.uk>
2016-08-11 20:56:23 +0100

    Hat -> hat

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ from setuptools import find_packages
 setup(
     name='Hat',
     version='0.0.1dev',
+    author='Qiuqiang Kong', 
+    author_email='qiuqiangkong@gmail.com', 
+    description='Deep learning toolbox built on Theano', 
+    url='https://github.com/qiuqiangkong/Hat', 
     packages=find_packages(),
     license='MIT',
 )
