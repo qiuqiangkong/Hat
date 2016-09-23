e53134329a688d8e75d6c31576b4a3e62a233c6a
Kong Q Mr (PG/R - Elec Electronic Eng) <qk00006@cvplws132.eps.surrey.ac.uk>
2016-09-23 14:38:08 +0100

    update examples/imdb_lstm

---
+++ examples/imdb_lstm/prepare_data.py
@@ -0,0 +1,80 @@
+'''
+SUMMARY:  preprocessing of imdb data
+          This file is from https://github.com/fchollet/keras/blob/master/keras/datasets/imdb.py
+AUTHOR:   Fchollet
+Created:  -
+Modified: 2016.05.27 modify return format
+--------------------------------------
+'''
+import os
+import cPickle
+import numpy as np
+
+'''
+Load imdb data, from https://github.com/fchollet/keras/blob/master/keras/datasets/imdb.py
+oov: out-of vocabulary
+'''
+def load_data(path="imdb.pkl", nb_words=None, skip_top=0,
+              maxlen=None, test_split=0.2, seed=113,
+              start_char=1, oov_char=2, index_from=3):
+
+    if not os.path.isfile(path):
+        from six.moves import urllib
+        print 'downloading data ... (15.3 Mb)'
+        urllib.request.urlretrieve( 'https://s3.amazonaws.com/text-datasets/imdb.pkl', path )
+
+    if path.endswith(".gz"):
+        f = gzip.open(path, 'rb')
+    else:
+        f = open(path, 'rb')
+
+    X, labels = cPickle.load(f)
+    f.close()
+
+    np.random.seed(seed)
+    np.random.shuffle(X)
+    np.random.seed(seed)
+    np.random.shuffle(labels)
+
+    if start_char is not None:
+        X = [[start_char] + [w + index_from for w in x] for x in X]
+    elif index_from:
+        X = [[w + index_from for w in x] for x in X]
+
+    if maxlen:
+        new_X = []
+        new_labels = []
+        for x, y in zip(X, labels):
+            if len(x) < maxlen:
+                new_X.append(x)
+                new_labels.append(y)
+        X = new_X
+        labels = new_labels
+    if not X:
+        raise Exception('After filtering for sequences shorter than maxlen=' +
+                        str(maxlen) + ', no sequence was kept. '
+                        'Increase maxlen.')
+    if not nb_words:
+        nb_words = max([max(x) for x in X])
+
+    # by convention, use 2 as OOV word
+    # reserve 'index_from' (=3 by default) characters: 0 (padding), 1 (start), 2 (OOV)
+    if oov_char is not None:
+        X = [[oov_char if (w >= nb_words or w < skip_top) else w for w in x] for x in X]
+    else:
+        nX = []
+        for x in X:
+            nx = []
+            for w in x:
+                if (w >= nb_words or w < skip_top):
+                    nx.append(w)
+            nX.append(nx)
+        X = nX
+
+    X_train = np.array(X[:int(len(X) * (1 - test_split))])
+    y_train = np.array(labels[:int(len(X) * (1 - test_split))])
+
+    X_test = np.array(X[int(len(X) * (1 - test_split)):])
+    y_test = np.array(labels[int(len(X) * (1 - test_split)):])
+
+    return X_train, y_train, X_test, y_test
