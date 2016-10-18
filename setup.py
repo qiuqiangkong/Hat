7582cc4d604457efd35dd830256b196d3135107f
Kong Q Mr (PG/R - Elec Electronic Eng) <qk00006@cvplws132.eps.surrey.ac.uk>
2016-10-18 11:38:09 +0100

    version dev0.0.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='Hat',
-    version='0.0.7dev',
+    version='0.0.8dev',
     author='Qiuqiang Kong', 
     author_email='qiuqiangkong@gmail.com', 
     description='Deep learning toolbox built on Theano', 
