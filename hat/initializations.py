6000082f555e75ad6b9d4e146ed4afcefa744cd8
Kong Q Mr (PG/R - Elec Electronic Eng) <qk00006@cvplws132.eps.surrey.ac.uk>
2017-02-19 17:43:10 +0000

    update annotation

--- hat/initializations.py
+++ hat/initializations.py
@@ -1,49 +1,40 @@
-'''
+"""
 SUMMARY:  initializations of weights
 AUTHOR:   Qiuqiang Kong
 Created:  2016.05.20
-Modified: 2016.08.02 modify all return to non shared
+Modified: 2017.02.19
 --------------------------------------
-'''
+"""
 import numpy as np
 import backend as K
 
-'''
-zeros tensor
-'''
+
 def zeros( shape ):
     return np.zeros( shape )
     
-'''
-ones tensor
-'''
 def ones( shape ):
     return np.ones( shape )
 
-'''
-identity matrix
-'''
 def eye( len ):
+    """Identity matrix
+    """
     return np.eye( len )
 
-'''
-uniform tensor
-'''
 def uniform( shape, scale=0.01 ):
     return np.random.uniform( -scale, scale, shape )
     
-'''
-glorot uniform
-[1] Glorot, Xavier, and Yoshua Bengio. "Understanding the difficulty of training deep feedforward neural networks." 2010.
-'''
-def glorot_uniform( shape ):
+
+def glorot_uniform( shape ):   
+    """
+    glorot uniform
+    [1] Glorot, Xavier, and Yoshua Bengio. "Understanding the difficulty of training deep feedforward neural networks." 2010.
+    """ 
     if len(shape)==2:
         fan_in, fan_out = shape[0], shape[1]
     elif len(shape)==4:
         receptive_field_size = shape[2]*shape[3]
         fan_in = shape[1] * receptive_field_size
         fan_out = shape[0] * receptive_field_size
-
     scale = np.sqrt( 6. / ( fan_in+fan_out ) )
     return uniform( shape, scale )
 
