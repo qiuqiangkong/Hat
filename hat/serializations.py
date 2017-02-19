a407eda58063d05013df645245019803710a3ecd
Kong Q Mr (PG/R - Elec Electronic Eng) <qk00006@cvplws132.eps.surrey.ac.uk>
2017-02-19 18:07:27 +0000

    update

--- hat/serializations.py
+++ hat/serializations.py
@@ -9,40 +9,37 @@ from supports import BFT
 import cPickle
 from layers.core import *
 from layers.cnn import *
-from layers.pool import *
+from layers.pooling import *
 from layers.rnn import *
 from layers.normalization import *
 from models import *
 
-### get data segment of model
-def get_model_data_seg( md ):
-    # find all nodes using BFT
-    id_list, layer_list = BFT( md.in_layers_ )
-    n_layers = len( id_list )
+
+def get_model_data_seg(md):
+    """Get data segment of model
+    """
+    effective_layers= md.get_effective_layers(md.in_layers_, md.out_layers_)
     
-    # construct dicts
     atom_list = []
-    for layer in layer_list:
+    
+    for layer in effective_layers:
         atom = {}
-        
-        # add data segment to atom
         atom['info'] = layer.info_
         
-        # add next_ids to atom
+        # pointer to next layers
         next_ids = []
         for next_layer in layer.nexts_:
-            next_ids.append( next_layer.id_ )
+            next_ids.append(next_layer.id_)
         atom['next_ids'] = next_ids
         
-        # add prev_ids to atom
+        # pointer to prev layers
         prev_ids = []
         for prev_layer in layer.prevs_:
-            prev_ids.append( prev_layer.id_ )
+            prev_ids.append(prev_layer.id_)
         atom['prev_ids'] = prev_ids
         
-        # add atom to list
-        atom_list.append( atom )
-        
+        atom_list.append(atom)
+    
     md_data_seg = {}
     md_data_seg['class_name'] = md.__class__.__name__
     md_data_seg['info'] = md.info_
@@ -50,77 +47,64 @@ def get_model_data_seg( md ):
     
     return md_data_seg
     
-### save model
-def save( md, path ):
+
+def save(md, path):
+    """Save model
+    """
     # get data segment of model
-    md_data_seg = get_model_data_seg( md )
+    md_data_seg = get_model_data_seg(md)
         
     # dump
-    cPickle.dump( md_data_seg, open( path, 'wb' ), protocol=cPickle.HIGHEST_PROTOCOL )
+    cPickle.dump(md_data_seg, open(path, 'wb'), protocol=cPickle.HIGHEST_PROTOCOL)
         
-### load model
-def load( path ):
-    # return atom from id
-    def _find_atom_( atom_list, id ):
-        for atom in atom_list:
-            if atom['info']['id'] == id:
-                return atom
-    
-    # return layer from id
-    def _find_layer( layer_list, id ):
+
+def load(path):
+    """load model
+    """
+    # search layer by id
+    def _find_layer(layer_list, id):
         for layer in layer_list:
             if layer.id_ == id:
                 return layer
     
-    # return all in_layers
-    def _find_in_layers( layer_list ):
-        in_layers = []
-        for layer in layer_list:
-            if not layer.prevs_:
-                in_layers.append( layer )
-        return in_layers
-    
-    # return all out_layers
-    def _find_out_layers( layer_list ):
-        out_layers = []
-        for layer in layer_list:
-            if not layer.nexts_:
-                out_layers.append( layer )
-        return out_layers
-    
-    
     # load serialized data
-    md_data_seg = cPickle.load( open( path, 'rb' ) )
+    md_data_seg = cPickle.load(open(path, 'rb'))
     
     md_info = md_data_seg['info']
-    md_func = globals().get( md_data_seg['class_name'] )
+    md_func = globals().get(md_data_seg['class_name'])
     atom_list = md_data_seg['atom_list']
     
-    # create layer from atom one by one
     layer_list = []
-    for id in xrange( len( atom_list ) ):
-        atom = _find_atom_( atom_list, id )
-        LayerClass = globals().get( atom['info']['class_name'] )
+    for atom in atom_list:
+        LayerClass = globals().get(atom['info']['class_name'])
         assert LayerClass is not None, "Try import '" + atom['info']['class_name'] + "' to serializations.py!"
-
+        
         if not atom['prev_ids']:
-            layer = LayerClass.load_from_info( atom['info'] )
+            layer = LayerClass.load_from_info(atom['info'])
         else:
             prev_ids = atom['prev_ids']
-            in_layers = [ _find_layer( layer_list, id ) for id in prev_ids ]
-            layer = LayerClass.load_from_info( atom['info'] )( in_layers )
-
-        layer_list.append( layer )
-        
-        
+            in_layers = [_find_layer(layer_list, id) for id in prev_ids]
+            layer = LayerClass.load_from_info(atom['info'])(in_layers)
+    
+        layer_list.append(layer)
+ 
     # find in_layers & out_layers
-    in_layers = [ _find_layer( layer_list, id ) for id in md_info['in_ids'] ]
-    out_layers = [ _find_layer( layer_list, id ) for id in md_info['out_ids'] ]
-    inter_layers = [ _find_layer( layer_list, id ) for id in md_info['any_ids'] ]
+    in_layers = [_find_layer(layer_list, id) for id in md_info['in_ids']]
+    out_layers = [_find_layer(layer_list, id) for id in md_info['out_ids']]
+    inter_layers = [_find_layer(layer_list, id) for id in md_info['any_ids']]
     
     # construct model
-    md = md_func.load_from_info( in_layers, out_layers, inter_layers, md_info )
+    md = md_func.load_from_info(in_layers, out_layers, inter_layers, md_info)
+    md.compile()
     
     return md
         
+
+def register(layer):
+    """Register user defined layer
+    
+    Args:
+      layer: user defined layer. 
+    """
+    exec(layer.__name__ + " = layer", locals(), globals())
         
