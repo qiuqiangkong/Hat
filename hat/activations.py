8a106b25cf81f9f6881e99af747aaeeee2d621e2
Kong Q Mr (PG/R - Elec Electronic Eng) <qk00006@cvplws132.eps.surrey.ac.uk>
2017-02-19 17:25:27 +0000

    add leaky_relu

--- hat/activations.py
+++ hat/activations.py
@@ -1,28 +1,27 @@
-'''
+"""
 SUMMARY:  activation functions
-          Pay attention if you are adding new activation, activation value 0. 
-          can only occur in 0-measure set. Because Masking use 0. to judge this should be masked or not. 
 AUTHOR:   Qiuqiang Kong
 Created:  2016.05.11
 Modified: -
 --------------------------------------
-'''
+"""
 import numpy as np
 import backend as K
 
 def linear( x ):
     return x
     
-# x can be tensor
 def sigmoid( x ):
+    """x can be tensor
+    """
     return K.sigmoid( x )
 
 def hard_sigmoid( x ):
     return K.hard_sigmoid( x )
 
-# x can be tensor
 def softmax( x ):
-    #assert x.ndim==2    # todo
+    """x can be tensor
+    """
     if x.ndim==2:
         return K.softmax( x )
     if x.ndim>2:
@@ -33,21 +32,24 @@ def softmax( x ):
 def tanh( x ):
     return K.tanh( x )
     
-    
-
 def relu( x, alpha=0., max_value=None ):
+    return K.relu( x, alpha, max_value )
+    
+def leaky_relu( x, alpha=0.2, max_value=None ):
     return K.relu( x, alpha, max_value )
     
 def zero( x ):
     return K.zeros_like(x)
-    
+  
 def get( act ):
     f = globals().get( act )
     if f is None:
         raise Exception( "No this activation!" )
     else:
         return f
+    
         
-### register user defined activation
 def register( act ):
+    """Register user defined activation. 
+    """
     exec( act.__name__ + " = act", locals(), globals() )
